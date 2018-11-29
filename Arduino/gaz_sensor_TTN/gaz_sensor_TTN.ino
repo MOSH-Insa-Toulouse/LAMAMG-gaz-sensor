f1f96af102dbaca4b93c399b471ac26912ee4c86
unknown <Adrien.MARTY-EXT@akka.eu>
2018-11-29 17:15:48 +0100

    Ajout de commentaires dans le code arduino

--- Arduino/gaz_sensor_TTN/gaz_sensor_TTN.ino
+++ Arduino/gaz_sensor_TTN/gaz_sensor_TTN.ino
@@ -2,14 +2,17 @@
 #include <TheThingsNetwork.h>
 #include <SoftwareSerial.h>
 
+// Creating a software serial
 SoftwareSerial loraSerial(8,9);
 #define debugSerial Serial
 
+// Variables' declaration
 float sensor_volt = 0;
 int LED = 13;
 volatile byte state = LOW;
-LiquidCrystal monEcran(12,11,7,6,5,4); // on crée l'objet écran
+LiquidCrystal myScreen(12,11,7,6,5,4); // creating the screen object
 
+// Data needed to connect to the TTN 
 const char *devAddr = "26011078";
 const char *nwkSKey = "E63AE401DD3BA8C23DA9670B93455FF3";
 const char *appSKey = "B28065B3C01B31621D5D0DED68B16511";
@@ -17,74 +20,48 @@ const char *appSKey = "B28065B3C01B31621D5D0DED68B16511";
 TheThingsNetwork ttn(loraSerial, debugSerial, TTN_FP_EU868);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Setting up the serial ports
   loraSerial.begin(57600);
   debugSerial.begin(9600);
 
-  ttn.onMessage(onMessageReceived);
-
+  // Asking for the TTN status
   debugSerial.println("-- STATUS");
   ttn.showStatus();
 
+  // Configuring the TTN
   debugSerial.println("-- PERSONALIZE");
   ttn.personalize(devAddr, nwkSKey, appSKey);
   ttn.personalize();
   debugSerial.println("-- PERSONALIZED");
-  
+
+  // Configuring the LED
   pinMode(LED,OUTPUT);
   pinMode(2,INPUT_PULLUP);
   digitalWrite(LED, LOW);
-  
-  monEcran.begin(16,2);
-  monEcran.clear();
-  monEcran.print("Gaz Sensor");
-  monEcran.setCursor(0, 1);
+
+  // Initializing the LCD screen
+  myScreen.begin(16,2);
+  myScreen.clear();
+  myScreen.print("Gaz Sensor");
+  myScreen.setCursor(0, 1);
+
+  // Creating a payload to be sent to the TTN
   byte payload[1];
   payload[0] = (digitalRead(2) == HIGH) ? 1 : 0;
   ttn.sendBytes(payload, sizeof(payload));
-  attachInterrupt(digitalPinToInterrupt(2), onButtonInterrupt, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Displaying the analog value of the sensor on the LCD screen
   sensor_volt = analogRead(A0)/1024.0*5.0;
-  //digitalWrite(LED, state);
-  monEcran.setCursor(0, 1);
-  monEcran.print(sensor_volt);
-  //ttn.poll();
-   
+  myScreen.setCursor(0, 1);
+  myScreen.print(sensor_volt);
+
+  // Sending the payload to the TTN
   byte payload[2];
   payload[0] = (digitalRead(2) == HIGH) ? 1 : 0;
   payload[1] = 100*sensor_volt;
-
-  // Send it off
   ttn.sendBytes(payload, sizeof(payload));
+  
   delay(5000);
-}
-
-void onButtonInterrupt()
-{
-  monEcran.setCursor(15, 1);
-  monEcran.print(digitalRead(2));
-
-  byte payload[1];
-  payload[0] = (digitalRead(2) == HIGH) ? 1 : 0;
-
-  // Send it off
-  //ttn.sendBytes(payload, 1);
-}
-
-void onMessageReceived(const uint8_t *payload, size_t size, port_t port)
-{
-  debugSerial.println("-- MESSAGE");
-  debugSerial.print("Received " + String(size) + " bytes on port " + String(port) + ":");
-
-  for (int i = 0; i < size; i++)
-  {
-    debugSerial.print(" " + String(payload[i]));
-  }
-
-  debugSerial.println();
-  //monEcran.clear();
-  //monEcran.write(payload, length);
 }
